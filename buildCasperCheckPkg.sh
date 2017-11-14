897bbffaed2c57b454ecb8a05978a22e0a851a97
Armin Briegel <ab@scriptingosx.com>
2017-11-14 13:45:55 +0100

    added components for prowarehouse

---
+++ buildCasperCheckPkg.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin
+
+# builds the 
+
+pkgname="CasperCheck"
+version="1.0"
+identifier="nl.prowarehouse.${pkgname}"
+install_location="/Library/Application Support/ProWarehouse/"
+signature="Installer: Armin Briegel"
+
+projectfolder=$(dirname "$0")
+
+pkgpath="${projectfolder}/${pkgname}-${version}.pkg"
+
+pkgbuild --root "${projectfolder}/script" \
+         --identifier "${identifier}" \
+         --version "${version}" \
+         --install-location "${install_location}" \
+         "${pkgpath}"
+
